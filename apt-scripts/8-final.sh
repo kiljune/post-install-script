6e2f8c938931fdd74393079efb9a23eba1b07f1a
Kiljune Choi <36096031+kiljune@users.noreply.github.com>
2025-03-22 21:50:07 +0900

    Add fltpak flathub remote

--- apt-scripts/8-final.sh
+++ apt-scripts/8-final.sh
@@ -67,6 +67,7 @@ if [[ ! -f /usr/local/bin/bat ]]; then
 fi
 sudo systemctl enable --now ufw
 sudo ufw enable
+sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 sudo virsh net-autostart default
 sudo apt autoremove
 sudo apt clean
